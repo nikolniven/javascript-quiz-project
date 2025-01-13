0c637860ba5cc98bcb834c7aa30a2696d7258ba6
Nikol_Niven <ntopalusic@gmail.com>
2025-01-13 17:50:59 +0100

    until 3 assignment

--- src/question.js
+++ src/question.js
@@ -1,7 +1,26 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+  constructor(text, choices, answer, difficulty) {
+    this.text = text;
+    this.choices = [...choices];
+    this.answer = answer;
+    this.difficulty = difficulty;
+  }
 
-    // 2. shuffleChoices()
+  shuffleChoices() {
+    for (let i = 0; i <= this.choices.length; i++) {
+      let j = Math.floor(Math.random() * this.choices.length);
+
+      const temp = this.choices[i];
+      this.choices[i] = this.choices[j];
+      this.choices[j] = temp;
+
+      return this.choices;
+    }
+  }
 }
+
+// const arrayTest = [1, 3, 6, 9, 1, 2, 123];
+
+// const randomIndex = Math.floor(Math.random() * arrayTest.length);
+
+// console.log("is random number:", randomIndex);
