24b1428be19f1f3961f2358f0aff1e5a465c1dbc
Nikol_Niven <ntopalusic@gmail.com>
2025-01-13 18:23:17 +0100

    quiz day 1 done

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,44 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+  // 2. getQuestion()
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
 
-    // 4. shuffleQuestions()
+  // 3. moveToNextQuestion()
+  moveToNextQuestion() {
+    return this.currentQuestionIndex++;
+  }
+  // 4. shuffleQuestions()
+  shuffleQuestions() {
+    for (let i = 0; i <= this.questions.length; i++) {
+      let j = Math.floor(Math.random() * this.questions.length);
 
-    // 5. checkAnswer(answer)
+      const temp = this.questions[i];
+      this.questions[i] = this.questions[j];
+      this.questions[j] = temp;
 
-    // 6. hasEnded()
+      return this.questions;
+    }
+  }
+  // 5. checkAnswer(answer)
+  checkAnswer(answer) {
+    if (answer) {
+      return this.correctAnswers++;
+    }
+  }
+  // 6. hasEnded()
+  hasEnded() {
+    if (this.currentQuestionIndex < this.questions.length) {
+      return false;
+    }
+    return true;
+  }
 }
